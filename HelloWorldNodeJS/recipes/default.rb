3ff26178ac5c811d3eafe5a565db0a3f0564d2da
jherbage <jasonherbage@googlemail.com>
2017-01-27 11:41:06 +0000

    HelloWorldApp

--- HelloWorldNodeJS/recipes/default.rb
+++ HelloWorldNodeJS/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+package 'nodejs' do
+  action :install
+end
+cookbook_file '/home/vagrant/hello.js' do
+  source 'hello.js'
+  mode 777
+  owner 'vagrant'
+  group 'vagrant'
+end
+
+
+service 'helloworldApp' do
+  action :start
+  status_command 'ps -ef | grep hello.js| grep -v grep'
+  start_command '/usr/bin/nodejs /home/vagrant/hello.js &' 
+  stop_command "kill -9 `ps -ef | grep hello.js | grep -v grep | awk '{print $2}' `"
+  restart_command "kill -9 `ps -ef | grep hello.js | grep -v grep | awk '{print $2}'` ; /usr/bin/nodejs /home/vagrant/hello.js &"
+end
+
